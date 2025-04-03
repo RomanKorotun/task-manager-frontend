57fcb95ca23c7dff68861b658bd358f213265ed5
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-04-03 20:47:49 +0300

    add electron module

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const App: FC = () => {
-  return <></>;
+  return <div></div>;
 };
 
 export default App;
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow, screen } from "electron";
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  mainWindow = new BrowserWindow({
+    width,
+    height,
+  });
+
+  mainWindow.loadURL("http://localhost:5173");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
